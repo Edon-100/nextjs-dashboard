c8b4415f6becb5e41fe7a5b5a0ead2e13e4dab7e
Edon- <540324745@qq.com>
2025-03-19 18:05:09 +0800

    add PPR

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,3 +1,27 @@
-export default function Page() {
-  return <p>Invoices Page</p>;
+import {
+  fetchCardData,
+  fetchLatestInvoices,
+  fetchRevenue,
+} from "@/app/lib/data";
+
+export default async function Page() {
+  const {
+    numberOfInvoices,
+    numberOfCustomers,
+    totalPaidInvoices,
+    totalPendingInvoices,
+  } = await fetchCardData();
+
+  const revenue = await fetchRevenue();
+  const latestInvoices = await fetchLatestInvoices();
+
+  return (
+    <p>
+      Invoices Page
+      {/* {numberOfInvoices}
+      {numberOfCustomers}
+      {totalPaidInvoices}
+      {totalPendingInvoices} */}
+    </p>
+  );
 }
